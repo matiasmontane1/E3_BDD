b26f79caed0655746d80bd0a45358dc93856070e
matiasmontane1 <matiasmontane1@uc.cl>
2024-11-14 18:25:55 -0300

    datos_db

--- config/data.php
+++ config/data.php
@@ -1,5 +1,5 @@
 <?php
-  $user = 'iuriarte17'; // acá va su grupo, ejemplo grupoXX
-  $password = 'Vasco.17'; // acá la contraseña del grupo
-  $databaseName = 'grupo61'; // acá la base de datos debe llamarse así: grupoXX
+  $user = 'grupo61e3'; // acá va su grupo, ejemplo grupoXX
+  $password = 'darwin'; // acá la contraseña del grupo
+  $databaseName = 'grupo61e3'; // acá la base de datos debe llamarse así: grupoXX
 ?>
