f7d2b4a6c3dafe82e00c635574ef9aab85669e07
IÑAKI URIARTE <iuriarte@uc.cl>
2024-11-15 16:53:58 -0300

    arreglo

--- temp_notas/transaccion.php
+++ temp_notas/transaccion.php
@@ -9,13 +9,19 @@ while (($linea = fgets($archivo_datos)) !== false) {
     $notas[] = explode(";", $linea);
 }
 fclose($archivo_datos);
-$crear_acta = "CREATE TEMP TABLE acta (
-    numero_alumno INT PRIMARY KEY REFERENCES Estudiantes(NumeroEstudiante),
-    asignatura VARCHAR(255) REFERENCES Cursos(Sigla),
-    periodo VARCHAR(12),
-    nombre_alumno VARCHAR(225),
-    nombre_profe VARCHAR(225),
-    nota_final FLOAT
-    );";
+try {
+     pg_query($db, "BEGIN");
+
+     $crear_acta = "CREATE TEMP TABLE acta (
+     numero_alumno INT PRIMARY KEY REFERENCES Estudiantes(NumeroEstudiante),
+     asignatura VARCHAR(255) REFERENCES Cursos(Sigla),
+     periodo VARCHAR(12),
+     nombre_alumno VARCHAR(225),
+     nombre_profe VARCHAR(225),
+     nota_final FLOAT
+     );";
+}
+
+
 
 ?>
