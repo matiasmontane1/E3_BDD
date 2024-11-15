b99a2678e4033dbc69dea38ffd23c60562b56f32
IÑAKI URIARTE <iuriarte@uc.cl>
2024-11-15 16:52:32 -0300

    inicio transaccion

--- temp_notas/transaccion.php
+++ temp_notas/transaccion.php
@@ -1,4 +1,21 @@
 <?php
-  require('../config/conexion.php');
-  
+require('../config/conexion.php');
+// leer datos de las notas
+$archivo_datos = fopen("../datos/notas_adivinacion |.csv", "r");
+$notas = [];
+$headers = fgets($archivo_datos);
+while (($linea = fgets($archivo_datos)) !== false) {
+    $linea = trim($linea);
+    $notas[] = explode(";", $linea);
+}
+fclose($archivo_datos);
+$crear_acta = "CREATE TEMP TABLE acta (
+    numero_alumno INT PRIMARY KEY REFERENCES Estudiantes(NumeroEstudiante),
+    asignatura VARCHAR(255) REFERENCES Cursos(Sigla),
+    periodo VARCHAR(12),
+    nombre_alumno VARCHAR(225),
+    nombre_profe VARCHAR(225),
+    nota_final FLOAT
+    );";
+
 ?>
