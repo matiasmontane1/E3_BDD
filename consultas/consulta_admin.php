24b1a52c93741c323f2e7e7a084235f34f9b2752
IÑAKI URIARTE <iuriarte@uc.cl>
2024-11-18 16:45:51 -0300

    l

--- consultas/consulta_admin.php
+++ consultas/consulta_admin.php
@@ -6,25 +6,19 @@
     $password = $_POST['password']; // Acá deben hashear la contraseña: https://www.php.net/manual/es/function.password-hash.php 
     $role = $_POST['role'];
 
-    $query = $db->prepare("SELECT * FROM usuario WHERE id = :id");
-    $query->bindParam(':id', $email);
-    $query->execute();
+    $query = "SELECT * FROM usuario WHERE id = '$email'";  // Usamos la variable directamente en la consulta (ten cuidado con la inyección SQL)
+    $result = pg_query($db, $query);
 
-    if ($query->rowCount() > 0) {
+    if (pg_num_rows($result) > 0) {
         echo "<p align='center'>El usuario ya está registrado en el sistema.</p>";
     } else {
-        $query = $db->prepare("INSERT INTO usuario (id, clave, rol) VALUES (:id, :clave, :rol)");
-        $query->bindParam(':id', $email);
-        $query->bindParam(':clave', $password);
-        $query->bindParam(':rol', $role);
+        $insert_query = "INSERT INTO usuario (id, clave, rol) VALUES ('$email', '$password', '$role')";
         
-        if ($query->execute()) {
+        if (pg_query($db, $insert_query)) {
             echo "<p align='center'>Usuario registrado con éxito.</p>";
         } else {
             echo "<p align='center'>Hubo un error al registrar el usuario. Intente nuevamente.</p>";
         }
     }
-  
-  ?>
-
+?>
 <?php include('../templates/footer_admin.html'); ?>
