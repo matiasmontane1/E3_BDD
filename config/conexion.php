ac552088cbb3eae057f78b16fd74499d0994999a
IÑAKI URIARTE <iuriarte@uc.cl>
2024-11-19 18:53:09 -0300

    creo que listo

--- config/conexion.php
+++ config/conexion.php
@@ -3,8 +3,8 @@
     #Pide las variables para conectarse a la base de datos.
     require('data.php'); 
     # Se crea la instancia de PDO
-    $db_profes = pg_connect("host=bdd1.ing.puc.cl port=5432 dbname=e3profesores user=grupo61e3 password=darwin");
-    $db = pg_connect("host=bdd1.ing.puc.cl port=5432 dbname=grupo61e3 user=grupo61e3 password=darwin");
+    $db_profes = pg_connect("host=localhost  port=5432 dbname=e3profesores user=grupo61e3 password=darwin");
+    $db = pg_connect("host=localhost  port=5432 dbname=grupo61e3 user=grupo61e3 password=darwin");
     
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
